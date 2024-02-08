8e3ed5e9e601928482cf435b2c3d3a42f357cc06
Leonardo Ledda <leonardoledda@gmail.com>
2024-02-08 12:46:10 +0100

    its: js: Initial express stuff
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ js/express/index.js
@@ -0,0 +1,39 @@
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const os = require('os');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200);
+    res.send('ciao');
+});
+
+app.get('/ciao', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200);
+    res.send('nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn');
+});
+
+app.get('/info', (req, res, next) => {
+    let string = os.version() + " " + os.arch() + "\ntotalmem: " +
+        (os.totalmem()/1024/1024/1024).toFixed(2) + "GB\nfreemem: " +
+        (os.freemem()/1024/1024/1024).toFixed(2) + "GB";
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200);
+    res.send(string);
+});
+
+app.post('/ciao', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200);
+    res.send('postato');
+});
+
+app.listen(3000);
+
