a6093e68cc2b6e8438da5991fbebc0436cfacf65
Leonardo Ledda <leonardoledda@gmail.com>
2024-02-14 17:23:49 +0100

    its: Fix pre-commit config
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- js/express/src/api/product/product.entity.ts
+++ js/express/src/api/product/product.entity.ts
@@ -1,10 +1,8 @@
 export interface Product {
-  id: string,
-  name: string,
-  description: string,
-  weight: number,
-  netPrice: number,
-  discount: number
+  id: string;
+  name: string;
+  description: string;
+  weight: number;
+  netPrice: number;
+  discount: number;
 }
-
-
