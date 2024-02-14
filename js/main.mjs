c9d89883aaac9d0f9dc0c4abb145d43e5aef2aa3
Leonardo Ledda <leonardoledda@gmail.com>
2024-02-14 15:43:59 +0100

    its: js: Add ts cart (incomplete)
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- js/main.mjs
+++
@@ -1,8 +0,0 @@
-import { readFile } from "fs";
-readFile('./ciao.txt', { encoding: 'utf-8' }, (err, content1) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(content1);
-    }
-});
